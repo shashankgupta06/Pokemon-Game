15b9031ea0cb92308a0c3d4772bd634f6a052dca
shashank gupta <shashank_32@live.com>
2018-06-13 22:47:32 -0400

    UI elements
    added more UI elements

---
+++ Assets/SignCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignCollider : MonoBehaviour {
+
+	public GameObject player;
+	public Text townText;
+	public GameObject canvas;
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (player.GetComponent<SpriteRenderer> ().sprite.name == "North_0") {
+			canvas.SetActive (true);
+			canvas.GetComponentInChildren<Text> ().text = gameObject.GetComponent<Text> ().text;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D c){
+		canvas.SetActive (false);
+	}
+}
