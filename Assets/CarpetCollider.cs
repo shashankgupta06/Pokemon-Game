438cacd675c77937aff3743ab7b42115b337b11a
shashank gupta <shashank_32@live.com>
2018-07-03 21:48:28 -0400

    Added more interaction
    few more features completed:
    
    Added more colliders in house
    connection between house and outside

---
+++ Assets/CarpetCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarpetCollider : MonoBehaviour {
+
+	public GameObject player;
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.gameObject.name == "player" && player.GetComponent<SpriteRenderer> ().sprite.name == "South_0") 
+		{
+			SceneManager.LoadScene ("main");
+
+		}
+
+
+	}
+}
---
+++ Assets/playerBottomHouseStairs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerBottomHouseStairs : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.gameObject.name == "player") 
+		{
+			SceneManager.LoadScene ("PlayerHouseTop");
+		}
+
+	}
+}
