1f6b9da56e9ae240dc32cd60c3a65245fbddc6d6
shashank gupta <shashank_32@live.com>
2018-07-09 13:02:36 -0400

    Colliders fixed in main scene
    Main scene objects colliding fine.
    Once the PLayers house is fixed too, i will pull changes to master

---
+++ Assets/PlayerHouseSignText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHouseSignText : MonoBehaviour {
+
+	string playerName;
+	public Text text;
+
+
+	void Awake()
+	{
+		playerName = PlayerPrefs.GetString ("PlayerName","");
+		text.text = playerName + "'s house";
+		Debug.Log (playerName);
+
+	}
+
+}
