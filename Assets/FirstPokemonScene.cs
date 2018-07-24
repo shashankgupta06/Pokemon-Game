def5e325a36bf7a30f29f5cbe13d5742883b5599
shashank gupta <shashank_32@live.com>
2018-07-23 22:58:55 -0400

    New scene added
    Ability to select first pokemon is on the horizon

---
+++ Assets/FirstPokemonScene.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstPokemonScene : MonoBehaviour
+{
+
+	public Sprite Bulbasaur, Charmander, Squirtle;
+	public Text pokemonName;
+	public Text pokemonBaseStats;
+	public Transform charmander;
+
+	private string pokemon;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		pokemon = PlayerPrefs.GetString ("FirstPokemon", "");
+		GameObject.Instantiate (charmander);
+		Debug.Log (pokemon);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		switch (pokemon) {
+
+		case "Squirtle":
+			gameObject.GetComponent<Image> ().sprite = Squirtle;
+			PokemonStats (pokemon);
+
+			break;
+
+		case "Charmander":
+			gameObject.GetComponent<Image> ().sprite = Charmander;
+			PokemonStats (pokemon);
+			break;
+
+		case "Bulbasaur":
+			gameObject.GetComponent<Image> ().sprite = Bulbasaur;
+			PokemonStats (pokemon);
+			break;
+
+		default:
+			break;
+
+		}
+		
+	}
+
+
+
+	void PokemonStats(string pokemon)
+	{
+
+		pokemonName.text = pokemon;
+
+		pokemonBaseStats.text = "HP: " + charmander.GetComponent<BasePokemon> ().HP + "\n" +
+		"Attack: " + charmander.GetComponent<BasePokemon> ().pokemonStats.attackStat + "\n" +
+		"Defence: " + charmander.GetComponent<BasePokemon> ().pokemonStats.defenceStat + "\n" +
+		"Sp.Attack: " + charmander.GetComponent<BasePokemon> ().pokemonStats.spAttackStat + "\n" +
+		"Sp.Defence: " + charmander.GetComponent<BasePokemon> ().pokemonStats.spDefenceStat + "\n" +
+		"speed: " + charmander.GetComponent<BasePokemon> ().pokemonStats.speedStat;
+
+	}
+}
---
+++ Assets/Scripts/FirstPokemon.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstPokemon : MonoBehaviour
+{
+
+	public GameObject player;
+	private string pokeball;
+
+	void OnTriggerEnter2D(Collider2D c){
+		pokeball = gameObject.name;
+
+	}
+
+	void Update()
+	{
+
+
+		if (player.GetComponent<BoxCollider2D> ().IsTouching (gameObject.GetComponent<BoxCollider2D> ()) && (player.GetComponent<SpriteRenderer>().sprite.name == "North_1" || player.GetComponent<SpriteRenderer>().sprite.name == "North_2") ) {
+			if(Input.GetKeyDown(KeyCode.Return)){
+				switch (pokeball) {
+
+				case  "pokeball":
+					Debug.Log ("Squirtle");
+					PokemonPickScene ("Squirtle");
+					break;
+				case "pokeball (3)":
+					Debug.Log ("Charmander");
+					PokemonPickScene ("Charmander");
+					break;
+				case "pokeball (1)":
+					Debug.Log ("Bulbasaur");
+					PokemonPickScene ("Bulbasaur");
+					break;
+
+					default:
+					break;
+
+
+				}
+
+
+			}
+
+		}
+
+	}
+
+
+
+	void PokemonPickScene(string pokemonName)
+	{
+		PlayerPrefs.SetString ("FirstPokemon", pokemonName);
+		SceneManager.LoadScene ("FirstPokemon");
+
+	}
+
+}
