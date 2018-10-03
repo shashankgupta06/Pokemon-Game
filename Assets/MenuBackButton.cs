aa02b338919ef2794cddab0792b0ca0f68ba43ce
shashank gupta <shashank_32@live.com>
2018-10-02 21:28:50 -0400

    update 110
    menu back button now working

---
+++ Assets/MenuBackButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBackButton : MonoBehaviour {
+
+	public Button backButton;
+	public GameObject player;
+
+	// Use this for initialization
+	void Awake () {
+		backButton.onClick.AddListener (TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void TaskOnClick() {
+		player.GetComponent<PlayerMovement> ().isAllowedToMove = true;
+		player.transform.Find ("Main Camera/Menu").gameObject.SetActive (false);
+	}
+}
